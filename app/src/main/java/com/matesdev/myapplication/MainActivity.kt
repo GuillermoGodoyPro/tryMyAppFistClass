84c4a3f65c9fd4174f69397beab7140c79377577
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-08-29 18:07:23 -0300

    toast added

--- app/src/main/java/com/matesdev/myapplication/MainActivity.kt
+++ app/src/main/java/com/matesdev/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.matesdev.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val miNombre : EditText =  findViewById(R.id.editTextTextNombre)
+        val boton : Button = findViewById(R.id.buttonAgregar)
+
+        val texto : String = miNombre.text.toString()
+
+        boton.setOnClickListener {
+            Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
